215ebf2287576b5c3f5891174f82cae0e0686220
Apple27022002 <100838894+Apple27022002@users.noreply.github.com>
2022-04-05 22:32:45 +0700

    m

--- src/kiemtra.java
+++ src/kiemtra.java
@@ -2,47 +2,80 @@ import java.util.Scanner;
 
 public class kiemtra {
     public static void main(String[] args) {
-            Scanner sc=new Scanner(System.in);
-            System.out.print("Nhap so luong : ");
-            int n = sc.nextInt();
-            System.out.println("Nhap cac phan tu: ");
-            int[] array = new int[n];
-            for (int i = 0; i < n; i++) {
-                System.out.print("nhap i" + i + " : ");
-                array[i] = sc.nextInt();
-            }
-            // tong chan
-            int tongchan = 0;
-            for (int i = 0; i < n; i++) {
-                if (array[i] % 2 == 0) {
-                    tongchan += array[i];
-                }
-            }
-            System.out.println("Tong chan : " + tongchan);
-            // tim vi tri a
-            System.out.print("Nhap vao a : ");
-            int a = sc.nextInt();
-            for (int i = 0; i < n; i++) {
-                if (array[i] == a) {
-                    System.out.print("Vi tri a la : " + i);
-                }
-            }
-            //nhap roi xoa a
-            int[] ad = new int[n - 1];
-            int c = 0;
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] == a) {
-                    c = i;
-                }
-            }
-            for (int i = 0; i < ad.length; i++) {
-                if (c < i) {
-                    ad[i] = array[i];
-                } else {
-                    ad[i] = array[i + 1];
-                }
-
-            }
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap vao N: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("nhap vao phan tu: " + i);
+            int number = scanner.nextInt();
+            arr[i] = number;
         }
+        displayArray(arr);
+        System.out.println("");
+        System.out.println("tong cac phan tu chan la: " + sum(arr));
+        System.out.println("nhap vao so can tim: ");
+        n = scanner.nextInt();
+        System.out.println("vi tri cua so can tim la: " + findNumber(n, arr));
+        System.out.println("nhap vao so muon xoa");
+        n = scanner.nextInt();
+        delElement(n, arr);
+        System.out.println("nhap vao so muon them:");
+        n = scanner.nextInt();
+        System.out.println("nhap vao vi tri muon them:");
+        int m = scanner.nextInt();
+        addElement(n, m, arr);
+    }
+    static int sum(int[] arr) {
+        int sum = 0;
+        for (int num :
+                arr) {
+            if (num % 2 == 0) {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+    static int findNumber(int num, int[] arr) {
+        int position = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (num == arr[i]) {
+                position = i;
+            }
+        }
+        return position;
+    }
+    static void delElement(int num, int[] arr) {
+        int[] arr2 = new int[arr.length - 1];
+        int fake = findNumber(num, arr);
+
+        if (fake != -1) {
+            for (int i = 0; i < fake ; i++) {
+                arr2[i] = arr[i];
+            }
+            for (int i = fake; i < arr2.length; i++) {
+                arr2[i] = arr[i+1];
+            }
+        }
+        displayArray(arr2);
+    }
+    static void addElement(int num,int position, int[] arr) {
+        String str = "";
+        int[] arr2 = new int[arr.length+1];
+        if (position >= 0) System.arraycopy(arr, 0, arr2, 0, position);
+        arr2[position] = num;
+        for (int i = position+1; i < arr2.length; i++) {
+            arr2[i] = arr[i-1];
+        }
+        displayArray(arr2);
+    }
+    static String  displayArray(int[] arr) {
+        System.out.printf("mang la: ");
+        String str = "";
+        for (int a: arr
+        ) {
+            str += System.out.printf( a + "\t");
+        }
+        return str;
+    }
 }
